d17a6de704e025ad8f74f6559b167126102c177d
Pedro Henrique Viana dos Santos <pedrohenry.viana@gmail.com>
2022-05-04 11:49:02 -0300

    [TEST]: add test for create feedback service

--- src/service/create-feedback.service.ts
+++ src/service/create-feedback.service.ts
@@ -1,6 +1,6 @@
-import { NodemailerMailAdapter } from "../adapters/nodemailer/nodemailer-mail.adapter";
-import { FeedbacksRepository } from "../repositories/models/feedbacks.repository";
-import { CreateFeedbackDto } from "./dto/create-feedback.dto";
+import { NodemailerMailAdapter } from '../adapters/nodemailer/nodemailer-mail.adapter';
+import { FeedbacksRepository } from '../repositories/models/feedbacks.repository';
+import { CreateFeedbackDto } from './dto/create-feedback.dto';
 
 export class CreateFeedbackService {
   constructor(
@@ -11,20 +11,32 @@ export class CreateFeedbackService {
   async execute(createFeedbackDto: CreateFeedbackDto) {
     const { type, comment, screenshot } = createFeedbackDto;
 
+    if (!type) {
+      throw new Error('Type is required');
+    }
+
+    if (!comment) {
+      throw new Error('Comment is required');
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot');
+    }
+
     await this.feedbacksRepository.create({
       type,
       comment,
-      screenshot,
+      screenshot
     });
 
     await this.mailAdapter.sendMail({
-      subject: "Feedback do usuário",
+      subject: 'Feedback do usuário',
       body: [
         `<div style="font-family: sans-serif; font-size= 16px; c olor=#111">`,
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
-        `</div>`,
-      ].join("\n"),
+        `</div>`
+      ].join('\n')
     });
   }
 }
